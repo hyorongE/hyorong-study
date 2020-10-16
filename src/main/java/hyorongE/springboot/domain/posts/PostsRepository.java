102d1c0ea9cd8d08aa21d3cde485c9e31c930faf
hyorongE <khj5910297@naver.com>
2020-10-16 19:07:37 +0900

    3장까지의 내용

---
+++ src/main/java/hyorongE/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package hyorongE.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+}
